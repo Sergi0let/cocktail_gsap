744cd147b70fc9d4383a365d9690ef093306d0de
Sergi0let <s.vashkevych@gmail.com>
2025-09-27 18:46:34 +0300

    Add Menu section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Art from "./components/Art";
 import Cocktails from "./components/Cocktails";
 import Hero from "./components/Hero";
+import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 
 //! register plugins in App.jsx for used globally
@@ -17,6 +18,7 @@ const App = () => {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
     </div>
   );
 };
